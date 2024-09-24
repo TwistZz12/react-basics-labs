e75b1257803bc39aac881539b1b684295a29d2fb
TwistZz12 <twistzzmeng@gmail.com>
2024-09-24 14:07:48 +0100

    Completed Tasky Lab 1

--- tasky/src/component/MyComponent.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-const Subject = () => {
-    return 
-    <p>
-        helloWorld
-    </p>
-}
-export default Subject;
---
+++ tasky/src/components/Task.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const Task = (props) => {
+    return (
+        <div className="card">
+            <p className='title'>{props.title}</p>
+            <p>Due：{props.deadline}</p>
+            <p>{props.children}</p>
+        </div>
+      
+    )
+
+}
+
+export default Task;
